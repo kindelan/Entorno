ae773d6e9a6648e34090e1bf59135c8644f18188
Ignacio Perez <kinderloon@gmail.com>
2018-03-21 23:29:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace CrearEntorno
+{
+    class Program
+    {
+
+            static void Main(string[] args)
+        {
+            string readText = File.ReadAllText(path: @"C:\Users\IGNACIO PEREZ\Desktop\Entorno.bat");
+            Console.WriteLine(readText);
+
+            string value = Environment.GetEnvironmentVariable("TestVariableName");
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+}
+    }
+    }
+
